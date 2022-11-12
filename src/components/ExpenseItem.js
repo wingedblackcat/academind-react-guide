c8bba376cebeac855726d3f10b1fd75ce513da3f
Michael Sayapin <michaelsay95@gmail.com>
2022-11-12 23:18:40 +0200

    writing more complex jsx code

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,12 @@
 const ExpenseItem = () => {
   return (
-    <h2>Expense Item!</h2>
+    <div>
+      <div>March th 2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$284.84</div>
+      </div>
+    </div>
   );
 };
 
