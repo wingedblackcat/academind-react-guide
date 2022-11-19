d4d4021545c3f407085cbba290d52ba5bdbe70e6
Michael Sayapin <michaelsay95@gmail.com>
2022-11-19 20:15:00 +0200

    splitting components into multiplle components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,24 +1,13 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 /**
- * @param {object} props 
- * @param {string} props.title
- * @param {string} props.amount
- * @param {Date} props.date
+ * @param {{title: string, amount: string, date: Date}} props 
  */
 const ExpenseItem = ({ title, amount, date }) => {
-  const month = date.toLocaleString('en-us', { month: 'long' });
-  const day = date.toLocaleString('en-us', { day: '2-digit' });
-  const year = date.getFullYear();
-
-
   return (
     <div className='expense-item'>
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={date} />
       <div className='expense-item__description'>
         <h2>{title}</h2>
         <div className='expense-item__price'>${amount}</div>
