aa0c4daa25a473aa2950b7b218cc39fecd82534f
Michael Sayapin <michaelsay95@gmail.com>
2022-11-20 21:10:19 +0200

    organizing component files

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,7 +1,7 @@
 import './Card.css';
 
 /**
- * @param {{ children: node|node[], className: string }} props  
+ * @param {{ children: node|node[], className?: string }} props  
  */
 const Card = ({ children, className }) => {
   return <div className={`card ${className}`}>{children}</div>;
